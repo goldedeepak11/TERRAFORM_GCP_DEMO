d257a30a3dca475771246ee3e29f7be9544e98a1
Deepak G <109938284+goldedeepak11@users.noreply.github.com>
2025-07-01 15:28:53 +0530

    Create variable.tf

---
+++ COMPUTE/GCE/variable.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  type        = string
+  description = "helpful-metric-459104-j4"
+}
+
+variable "region" {
+  type        = string
+  default     = "us-central1"
+  description = "GCP Region"
+}
+
+variable "zone" {
+  type        = string
+  default     = "us-central1-a"
+  description = "GCP Zone"
+}
